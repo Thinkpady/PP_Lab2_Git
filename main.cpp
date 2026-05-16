393f789bd9556a4f9e7c77fdcce91481a70aebbe
thinkpady <golovachevaleksandr2007@gmail.com>
2026-05-16 20:28:25 +0300

    Add English

--- main.cpp
+++ main.cpp
@@ -3,17 +3,30 @@
 
 using namespace std;
 
-void show_menu(){
-    cout << "[--------Главное меню--------]\n";
-    cout << "1 - Посмотреть таблицу по ученикам\n";
-    cout << "2 - Добавить ученика\n";
-    cout << "3 - Добавить данные ученика\n";
-    cout << "4 - Сформировать отчет по проведенным урокам\n";
+void show_menu(int mode){
+    // 1 - russian, english
+    if (mode == 0){
+        cout << "[--------Главное меню--------]\n";
+        cout << "1 - Посмотреть таблицу по ученикам\n";
+        cout << "2 - Добавить ученика\n";
+        cout << "3 - Добавить данные ученика\n";
+        cout << "4 - Сформировать отчет по проведенным урокам\n";
+    }
+
+    else if (mode == 1){
+        cout << "[--------Main menu--------]\n";
+        cout << "1 - View the table for students\n";
+        cout << "2 - Add a student\n";
+        cout << "3 - Add Student data\n";
+        cout << "4 - Generate a report on the lessons learned\n";
+    }
 }
+
+
 
 int main(){
     setlocale(LC_ALL, "ru_RU.UTF-8");
-    show_menu();
+    show_menu(1);
     return 0;
 }
 
