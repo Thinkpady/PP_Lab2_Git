fa6b255f83927284c7a4a71d487198c08a81d7fc
thinkpady <golovachevaleksandr2007@gmail.com>
2026-05-16 20:32:38 +0300

    Add dot

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ void show_menu(int mode){
     // 1 - russian, english
     if (mode == 0){
         cout << "[--------Главное меню--------]\n";
-        cout << "1 - Посмотреть таблицу по ученикам\n";
-        cout << "2 - Добавить ученика\n";
-        cout << "3 - Добавить данные ученика\n";
-        cout << "4 - Сформировать отчет по проведенным урокам\n";
+        cout << "1 - Посмотреть таблицу по ученикам.\n";
+        cout << "2 - Добавить ученика.\n";
+        cout << "3 - Добавить данные ученика.\n";
+        cout << "4 - Сформировать отчет по проведенным урокам.\n";
     }
 
     else if (mode == 1){
         cout << "[--------Main menu--------]\n";
-        cout << "1 - View the table for students\n";
-        cout << "2 - Add a student\n";
-        cout << "3 - Add Student data\n";
-        cout << "4 - Generate a report on the lessons learned\n";
+        cout << "1 - View the table for students.\n";
+        cout << "2 - Add a student.\n";
+        cout << "3 - Add Student data.\n";
+        cout << "4 - Generate a report on the lessons learned.\n";
     }
 }
 
